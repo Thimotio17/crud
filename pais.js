22ccdf9ade8978ca018c8920a93aaf5c542b4588
thimotio <thimotio998@gmail.com>
2024-07-25 11:12:12 -0300

    refactor

--- pais.js
+++ pais.js
@@ -1,1 +1,54 @@
-prompt-
+const prompt = require("prompt-sync")
+
+const paises = []
+
+
+const ValidarPais = pais => pais.nome != "" && sigla.length == 2 
+
+
+const modelo = () => {
+    const nome = prompt("Digite o nome do país: ")
+    const sigla = prompt ("Digite a sigla do paaís: ").toUpperCase()
+    
+    if(ValidarPais({nome, sigla})){
+   return{nome, sigla}
+    }
+
+    console.log("Dados invalidos!")
+}
+
+
+const criarPais = () => {
+    const pais = modelo()
+
+    if(pais != undefined){
+        pais.push({nome, sigla})
+console.log("Pais criado com sucesso!")
+    }
+    
+}
+
+
+const listarPais =  () => {
+    if(pais.length == 0){
+        console.log("Nenhum pais cadastrado!")
+    }else{
+        
+        paises.forEach((pais, indice)=> {
+            console.log(indice + 1, pais)
+
+        })
+    }}
+
+    const atualizarPais = () => {
+
+listarPais()
+if (paises.length > 0 ){
+
+const indice = prompt("Digite o pais que deseja atualizar: ")
+
+
+}
+
+
+    }
